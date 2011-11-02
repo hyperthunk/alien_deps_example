4a0371b3f1ad1dfc4fc1d33c248fbb914ec5464e
Tim Watson <watson.timothy@gmail.com>
2011-11-02 15:27:37 +0000

    Integrate libconf to provide control over generated release config; add bootstrap

--- README.md
+++ README.md
@@ -1,11 +1,19 @@
 ## Sample
 
-Install the prerequisites like so:
+Boostrap the build first:
+
+    $ ./bootstrap
+
+Then configure:
+
+    $ ./configure --with-erlydtl=build/deps/erlydtl/ebin --verbose --nocache
+
+Then we install the prerequisites like so:
 
     ./rebar skip_deps=true get-deps install-plugins install-deps
 
-Then generate a release:
+Then we generate a release:
 
     ./rebar -C release.config generate
 
-Very experimental. Requires embedded version of rebar.
+Very experimental.
---
+++ build/src/configure.erl
@@ -0,0 +1,59 @@
+%% -----------------------------------------------------------------------------
+%%
+%% Copyright (c) 2011 Tim Watson (watson.timothy@gmail.com)
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy
+%% of this software and associated documentation files (the "Software"), to deal
+%% in the Software without restriction, including without limitation the rights
+%% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+%% copies of the Software, and to permit persons to whom the Software is
+%% furnished to do so, subject to the following conditions:
+%%
+%% The above copyright notice and this permission notice shall be included in
+%% all copies or substantial portions of the Software.
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+%% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+%% THE SOFTWARE.
+%% -----------------------------------------------------------------------------
+-module(configure).
+
+-include_lib("libconf/include/libconf.hrl").
+-compile(export_all).
+
+main(Args) ->
+    libconf:configure(Args, options(), rules()).
+
+options() ->
+    [{"--(?<option>.*)=(?<value>.*)", fun erlang:list_to_tuple/1,
+        [option, value],
+        [{"prefix",
+            "Base install directory - defaults to the release folder", "./"},
+         {"resolve_format", "resolve-conf format (see "
+            "http://www.erlang.org/doc/apps/erts/inet_cfg.html for details)",
+            "resolv"},
+         {"resolve_file", "location of the resolve file", 
+            "/etc/resolv.conf"}]}].
+
+rules() ->
+    [{checks, [
+        #check{ type=rebar, name=rebar, mandatory=true,
+                capture=filename:join("build", "deps"),
+                data={ %"git://github.com/hyperthunk/rebar.git", 
+                       "/Users/t4/work/hyperthunk/rebar",
+                       "pub-cmd-alt-deps" } }
+     ]},
+     {templates, [
+        #template{ name=rebar, output="rebar",
+                   overwrite=true, checks=[rebar] },
+        #template{ name=release_vars, overwrite=true,
+                   output="release/release.vars" }]},
+     {actions, [
+        #action{ type=chmod, target="rebar",
+                 config={mode, 8#00100},
+                 checks=[rebar] }
+     ]}].
