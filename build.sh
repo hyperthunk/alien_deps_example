b13f799ff585205ae495c8bd83f9793e24ab5e73
Tim Watson <watson.timothy@gmail.com>
2011-11-04 16:08:09 +0000

    make build.sh verbose

--- build.sh
+++ build.sh
@@ -7,15 +7,15 @@
 rebar configure \
     libconf.erlydtl=build/deps/erlydtl/ebin \
     libconf.nocache=true \
-    libconf.verbose=true
+    libconf.verbose=true -v
 
 # TODO: add a quiet=true option to rebar_alien_deps 
 # for redirecting stdout to dev/null where possible
 
 # gets all required plugins and installs all deps (including ejabberd)
-./rebar skip_deps=true get-deps install-plugins install-deps
+./rebar skip_deps=true get-deps install-plugins install-deps -v
 
 # generates a release, with the pre_release_plugin doing some extra pre/post 
 # processing work in order to fix various suprious artefacts of process-one's
 # decision to package ejabberd in a rather un-OTP manner
-./rebar -C release.config generate
+./rebar -C release.config generate -v
